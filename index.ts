46166c034975b03fc4318aa5ee33e144db51e737
tony-elvis <124935722+tony-elvis@users.noreply.github.com>
2023-11-17 06:58:20 -0800

    refactor(app): refactor code

--- index.ts
+++ index.ts
@@ -99,6 +99,28 @@ following properties: */
             dev.executionDate = new Date().toLocaleDateString();
         });
     }
+
+    /**
+    * The function "getActivesTeamMembers" returns an array of team members who have an "Active"
+    * status.
+    * @returns an array of team members who have a status of "Active".
+    */
+    getActivesTeamMembers(){
+        return this.teamMember.filter(dev => dev.Status === 'Active');
+    }
+
+    /**
+     * The function returns a list of active team members ordered by the specified properties.
+     * @param {properties} properties - The "properties" parameter is an object that specifies the
+     * properties by which the active team members should be ordered. Each property should be a
+     * key-value pair, where the key is the property name and the value is the sorting order (either
+     * "asc" for ascending or "desc" for descending).
+     * @returns a list of active team members ordered by the specified properties.
+     */
+    getActivesMembersOrderedByProper(properties: properties){
+        const filterData = this.getActivesTeamMembers();
+        return TeamClass.orderByObjectProperty(filterData, properties)
+    }
   
     /**
      * The function returns the team member.
@@ -115,35 +137,17 @@ following properties: */
      * "Person" objects in the array that you want to sort by.
      * @returns the sorted array of Person objects based on the specified property.
      */
-    private  static orderByObjectProperty(array: Person[], property: properties) {
+    public static orderByObjectProperty(array: Person[], property: properties) {
         return array.sort((a, b) => (a[property] || '').localeCompare(b[property] || ''));
-      }
-
-    /**
-     * The function "showActivesTeamMembers" filters the team members based on their status, orders
-     * them by a specified property, and then displays the ordered list.
-     * @param {properties} property - The "property" parameter is a property of the team members that
-     * you want to use for ordering the active team members.
-     * @returns an array of active team members.
-     */
-    showActivesTeamMembers(property: properties){
-        const ActivesTeamMembers = this.teamMember.filter(dev => dev.Status === 'Active');
-        if(property && ActivesTeamMembers.length > 0){
-            const ActivesTeamMembersOrdered = TeamClass.orderByObjectProperty(ActivesTeamMembers, property)
-            TeamClass.showMember(ActivesTeamMembersOrdered);
-            return ActivesTeamMembers;
-        } 
-        
-        console.log('No team members actives found');
-        return ActivesTeamMembers;
-    }
+      }    
+   
 
     /**
      * The function "showMember" takes an array of Person objects and logs information about each
      * person's name, execution date, and favorite movie.
      * @param {Person[]} array - An array of objects of type Person.
      */
-    private static showMember(array:  Person[]) {
+    public static showMember(array:  Person[]) {
         array.forEach(obj => {
             console.log(`Developer info: 
             name ${obj.Name}, 
@@ -151,11 +155,39 @@ following properties: */
             Favorite movie: ${obj.favorite_movie}`);
         });
     }
+
+    public static getMemberInfo(array:  Person[]) {
+        return array.map(obj => {
+            return {
+                Name: obj.Name,
+                executionDate: obj.executionDate,
+                favorite_movie: obj.favorite_movie
+            };
+        });
+    }
+  }
+
+  /**
+   * The function `getFormatData` retrieves active members from a team and displays their information.
+   * @returns The function `getFormatData` is returning the result of calling the `showMember` method
+   * of the `TeamClass` class, passing in the `activeMembers` array as an argument.
+   */
+  const getFormatData = ()=>{
+    const activeMembers: Person[] = team.getActivesMembersOrderedByProper(properties.Name);
+    return TeamClass.getMemberInfo(activeMembers);
   }
   
+
+  // implementation
   const team = new TeamClass();
   team.addTeamMember(tonyObj);
   team.addExecutionDate();
-  team.showActivesTeamMembers(properties.Name)
+  const activeMembers = team.getActivesMembersOrderedByProper(properties.Name);
+
+ if(activeMembers.length > 0){
+    TeamClass.showMember(activeMembers);
+    }else{
+        console.log('No team members actives found');
+    } 
  
   
